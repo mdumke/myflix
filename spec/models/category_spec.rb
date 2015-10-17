b1b21fe3323f65d64675e0a7bd5cf62937c28334
Matthias Dumke <matthias.dumke@gmx.net>
2015-10-17 18:50:05 +0200

    rewrite tests using shoulda-matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,32 +1,7 @@
 require 'spec_helper'
 
 describe Category do
-  it 'saves itself' do
-    cat = Category.new(name: 'Existencial Drama')
-
-    cat.save
-
-    expect(Category.first).to eq cat
-  end
-
-  it 'can have multiple videos' do
-    v1 = Video.new(
-      title: 'Ghostbusters',
-      description: 'something with ghosts')
-
-    v2 = Video.new(
-      title: 'I was born but...',
-      description: 'something with a boy')
-
-    Category.create(name: 'test', videos: [v1, v2])
-  
-    expect(Category.first.videos).to include v1, v2
-  end
-
-  it 'is invalid without a name' do
-    category = Category.new
-
-    expect(category.valid?).to be_falsy
-  end
+  it { should validate_presence_of(:name) }
+  it { should have_many(:videos) }
 end
 
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,39 +1,9 @@
 require 'spec_helper'
 
 describe Video do
-  it 'saves correctly' do
-    video = Video.new(
-      title: 'Dirty Harry',
-      description: 'Do you feel lucky, Punk?')
+  it { should belong_to(:category) }
 
-    video.save
-
-    expect(Video.find_by_title('Dirty Harry')).to eq video
-  end
-
-  it 'can have a category' do
-    category = Category.new(name: 'Western')
-    
-    video = Video.new(
-      title: 'Stagecoach',
-      description: 'nice and not weird movie',
-      category: category)
-
-    video.save
-
-    expect(Video.first.category).to eq category
-  end
-
-  it 'is invalid without a title' do
-    video = Video.new(description: 'a movie about something important')
-
-    expect(video.valid?).to be_falsy
-  end
-
-  it 'is invalid without a description' do
-    video = Video.new(title: 'Audition')
-
-    expect(video.valid?).to be_falsy
-  end
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 end
 
