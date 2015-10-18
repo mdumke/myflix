134bba64c42e651edc2a3b6c0ea9d67e63c1c615
Matthias Dumke <matthias.dumke@gmx.net>
2015-10-18 18:29:31 +0200

    add a uniqueness-validation for the user-email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ describe User, type: :model do
   it { should validate_presence_of(:full_name) }
   it { should validate_presence_of(:password) }
   it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
 
   it 'does not complain about missing confirmation' do
     u = User.create(
