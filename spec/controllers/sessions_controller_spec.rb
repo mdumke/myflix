e25d36c252854f4d476359006271684acbbf9979
Matthias Dumke <matthias.dumke@gmx.net>
2015-10-24 12:42:44 +0200

    refactor controller tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe SessionsController do
   describe 'GET front' do
-    it 'should render the front template for unauthenticated users' do
+    it 'renders the front template for unauthenticated users' do
       get :front
       expect(response).to render_template :front
     end
 
-    it 'should redirect to the home-path for authenticated users' do
+    it 'redirects to the home-path for authenticated users' do
       session[:user_id] = Fabricate(:user).id
       get :front
       expect(response).to redirect_to home_path
@@ -15,12 +15,12 @@ describe SessionsController do
   end
 
   describe 'GET new' do
-    it 'should render the new template for unauthenticated users' do
+    it 'renders the new template for unauthenticated users' do
       get :new
       expect(response).to render_template :new
     end
 
-    it 'should redirect to the home-path for authenticated users' do
+    it 'redirects to the home-path for authenticated users' do
       session[:user_id] = Fabricate(:user).id
       get :new
       expect(response).to redirect_to home_path
@@ -28,39 +28,61 @@ describe SessionsController do
   end
 
   describe 'POST create' do
-    let (:user) { Fabricate(:user) }
+    let (:alice) { Fabricate(:user) }
 
-    it 'logs in user with valid email and password' do
-      post :create, email: user.email, password: user.password
-      expect(session[:user_id]).to eq user.id
+    context 'with valid credentials' do
+      before do
+        post :create, email: alice.email, password: alice.password 
+      end
+
+      it 'puts the user in the session' do
+        expect(session[:user_id]).to eq alice.id
+      end
+
+      it 'redirects to the videos-path' do
+        expect(response).to redirect_to videos_path 
+      end
+
+      it 'sets the notice' do
+        expect(flash['notice']).not_to be_blank
+      end
     end
 
-    it 'redirects to videos-path for a user with valid email and password' do
-      post :create, email: user.email, password: user.password
-      expect(response).to redirect_to videos_path 
-    end
+    context 'with invalid credentials' do
+      before do
+        post :create, email: alice.email, password: alice.password + 'asdf'
+      end
 
-    it 'does not log in user with invalid email' do
-      post :create, email: 'another@email', password: user.password
-      expect(session[:user_id]).to be_nil 
-    end
+      it 'does not put the user in the session' do
+        expect(session[:user_id]).to be_nil
+      end
 
-    it 'does not log in user with invalid password' do
-      post :create, email: user.email, password: 'false'
-      expect(session[:user_id]).to be_nil 
-    end
+      it 'renders the new view if email or password are invalid' do
+        expect(response).to render_template :new
+      end
 
-    it 'renders the new view if email or password are invalid' do
-      post :create, email: user.email, password: 'false'
-      expect(response).to render_template :new 
+      it 'sets the error-message' do
+        expect(flash['error']).not_to be_blank
+      end
     end
   end
 
   describe 'GET destroy' do
-    it 'logs out signed in user' do
+    before do
       session[:user_id] = Fabricate(:user).id
       get :destroy
+    end
+
+    it 'logs out signed in user' do
       expect(session[:user_id]).to be_nil
+    end
+
+    it 'redirects to the root-path' do
+      expect(response).to redirect_to root_path
+    end
+
+    it 'sets the notice' do
+      expect(flash['notice']).not_to be_blank
     end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,38 +2,44 @@ require 'spec_helper'
 
 describe UsersController do
   describe 'GET new' do
-    it 'sets the @user variable' do
+    it 'sets @user' do
       get :new
+      expect(assigns(:user)).to be_a User
       expect(assigns(:user)).to be_a_new_record
     end
   end
 
   describe 'POST create' do
-    let (:full_name) { Faker::Name.name }
-    let (:password)  { Faker::Internet.password(8) }
-    let (:email)     { Faker::Internet.email }
+    context 'with valid input' do
+      before do
+        post :create, user: Fabricate.attributes_for(:user)
+      end
 
-    it 'creates a user when data is valid' do
-      post :create, user: {full_name: full_name, password: password, email: email}
+      it 'creates a user' do
+        expect(User.count).to eq 1
+      end
 
-      expect(User.first.full_name).to eq full_name
-      expect(User.first.email).to eq email
+      it 'redirects to videos path' do
+        expect(response).to redirect_to videos_path 
+      end
     end
 
-    it 'redirects to videos path when data is valid' do
-      post :create, user: {full_name: full_name, password: password, email: email}
+    context 'with invalid input' do
+      before do
+        post :create, user: {full_name: '', password: '123'} 
+      end
 
-      expect(response).to redirect_to videos_path 
-    end
+      it 'does not create a new user' do
+        expect(User.count).to eq 0
+      end
 
-    it 'does not create a new user when data is invalid' do
-      post :create, user: {full_name: '', password: '123'}
-      expect(User.all.size).to eq 0
-    end
+      it 'renders the new view' do
+        expect(response).to render_template :new 
+      end
 
-    it 'renders the new view when data is invalid' do
-      post :create, user: {full_name: '', password: '123'}
-      expect(response).to render_template :new 
+      it 'sets @user' do
+        expect(assigns(:user)).to be_a User
+      end
     end
   end
 end
