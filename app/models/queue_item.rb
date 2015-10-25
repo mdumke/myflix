c422fa28308a6752206c32c56677f49681b0d9e5
Matthias Dumke <matthias.dumke@gmx.net>
2015-10-25 11:49:01 +0100

    add queue item methods for easier data access

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,5 +1,22 @@
 class QueueItem < ActiveRecord::Base
   belongs_to :user
   belongs_to :video 
+
+  def video_title
+    video.title
+  end
+
+  def category_name
+    video.category.name
+  end
+
+  def category
+    video.category
+  end
+
+  def rating
+    review = Review.where(user: user, video: video).first
+    review ? review.rating : nil
+  end
 end
 
--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -3,5 +3,35 @@ require 'spec_helper'
 describe QueueItem do
   it { should belong_to(:user) }
   it { should belong_to(:video) }
+
+  describe 'getting the rating for a video' do
+    let (:alice) { Fabricate(:user) }
+    let (:video) { Fabricate(:video) }
+
+    context 'user has rated a video' do
+      it 'returns the rating' do
+        review1 = Fabricate(:review, rating: 3, text: 'abc', user: alice, video: video)
+        queue_item = Fabricate(:queue_item, user: alice, video: video)
+
+        expect(queue_item.rating).to eq 3 
+      end
+
+      it 'returns the first rating if there are many' do
+        review1 = Fabricate(:review, rating: 3, text: 'abc', user: alice, video: video)
+        review2 = Fabricate(:review, rating: 3, text: 'abc', user: alice, video: video)
+        queue_item = Fabricate(:queue_item, user: alice, video: video)
+
+        expect(queue_item.rating).to eq 3 
+      end
+    end
+
+    context 'user has not yet rated a video' do
+      it 'returns nil' do
+        queue_item = Fabricate(:queue_item, user: alice, video: video)
+          
+        expect(queue_item.rating).to be_nil
+      end
+    end
+  end
 end
 
