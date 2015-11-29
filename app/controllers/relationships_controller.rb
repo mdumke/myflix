4c89f63469ef65d11bbe199ce805b4d02678dee6
Matthias Dumke <matthias.dumke@gmx.net>
2015-11-29 13:24:53 +0100

    refactor relationships controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,26 +1,18 @@
 class RelationshipsController < ApplicationController
   before_action :require_user, only: [:index, :destroy]
-  before_action :set_relationship, only: [:destroy]
 
   def index
     @follower_relationships = current_user.follower_relationships
   end
 
   def destroy
-    @relationship.destroy
-    flash[:notice] = "You have unfollowed #{@relationship.leader.full_name}."
-    redirect_to people_path
-  end
+    relationship = Relationship.find_by_id(params[:id])
 
-  private
-
-  def set_relationship
-    @relationship = current_user.follower_relationships.find_by_id(params[:id])
-
-    unless @relationship
-      flash[:error] = 'This relationship could not be found'
-      redirect_to home_path
+    if relationship.follower == current_user
+      relationship.destroy
+      flash[:notice] = "You have unfollowed #{relationship.leader.full_name}."
     end
+    redirect_to people_path
   end
 end
 
