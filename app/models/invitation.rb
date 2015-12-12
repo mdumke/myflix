9d31289e0e2b4f3213016b4a17d13675f660fec7
Matthias Dumke <matthias.dumke@gmx.net>
2015-12-12 16:10:50 +0100

    refactor token generation into concern

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,15 +1,9 @@
 class Invitation < ActiveRecord::Base
+  include Tokenable
+
   belongs_to :inviter, class_name: 'User'
 
   validates :inviter, :recipient_name, :recipient_email, :message,
             presence: true
-
-  before_create :generate_token
-
-  private
-
-  def generate_token
-    self.token = SecureRandom.urlsafe_base64
-  end
 end
 
